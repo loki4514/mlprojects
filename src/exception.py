274fc2d7e101776ee89a920b6bec7db221905ef0
lokesh4514 <60394592+lokesh4514@users.noreply.github.com>
2023-03-17 20:36:11 +0530

    files are added

---
+++ src/exception.py
@@ -0,0 +1,50 @@
+import sys
+# that provides access to some system-specific functionalities and variables. 
+# It allows Python code to interact with the underlying operating system and to, 
+# perform tasks like reading and writing to the standard input/output/error 
+
+
+def error_message_details(error,error_details:sys):
+    # only last info is enough 
+    # info gives all the info about expection in while file on which line
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_msg = str(error)
+    error_message = f"Error Occured in python script name [{file_name}] line number \
+        [{line_number}] error message [{error_msg}]"
+        
+        
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        # overwriring with parent class 
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details=error_details)
+        
+    def __str__(self):
+        return self.error_message
+    
+    
+    
+    
+############ or ################################3
+# class CustomException(Exception):
+#     def __init__(self, error_message, error_details):
+#         super().__init__(error_message)
+#         _, _, exc_tb = error_details.exc_info()
+#         file_name = exc_tb.tb_frame.f_code.co_filename
+#         line_number = exc_tb.tb_lineno
+#         self.error_message = f"Error occurred in {file_name}, line {line_number}: {error_message}"
+
+#     def __str__(self):
+#         return self.error_message
+
+
+if __name__ == "__main__":
+    # try:
+    #     a = 1/0
+    # except Exception as e:
+    #     raise CustomException(e,sys)
+    
---
+++ src/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+# creating a log file where error ha occured and  many more 
+Log_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# strftime : data time object into string
+
+logs_path = os.path.join(os.getcwd(),"logs",Log_FILE)
+
+# crate directory to append the logs
+os.makedirs(logs_path,exist_ok=True)
+
+Log_FILE_path = os.path.join(logs_path,Log_FILE)
+
+logging.basicConfig(
+    filename=Log_FILE_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info("Logging started ")
