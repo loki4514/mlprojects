0b048dee0e993ee16b808f5d0e17b747abe2326d
lokesh4514 <60394592+lokesh4514@users.noreply.github.com>
2023-04-04 20:27:40 +0530

    deployment config

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -43,7 +43,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    # data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation.initiate_data_transformation(train_data,test_data)
     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
     
     modelTrainer = ModelTrainer()
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill
+import dill,pickle
 
 from sklearn.metrics import r2_score
 
@@ -45,3 +45,11 @@ def evaluate_model(x_train,y_train,x_test,y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
